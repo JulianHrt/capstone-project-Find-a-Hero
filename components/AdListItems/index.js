e6cb3d7cebefa7f970fa03f1c1344fb228025bbf
Julian Hirt <julian.hirt94@web.de>
2022-11-17 10:58:43 +0100

    done refactoring while waiting

--- components/AdListItems/index.js
+++ components/AdListItems/index.js
@@ -24,20 +24,22 @@ export default function AdListItems({
         ></Image>
       </ImageContainer>
       <h2>{adTitle}</h2>
-      <UserContainer>
-        <UserProfilPhoto
-          src={
-            userPictureSrc == ""
-              ? `https://source.unsplash.com/random/?person ${userName}`
-              : userPictureSrc
-          }
-          width={40}
-          height={40}
-          alt={`Profilphoto of ${userName}`}
-        ></UserProfilPhoto>
-        <p>{userName}</p>
-      </UserContainer>
-      <Costs>Costs: {adCosts} Karmapoints</Costs>
+      <Description>
+        <UserContainer>
+          <UserProfilPhoto
+            src={
+              userPictureSrc == ""
+                ? `https://source.unsplash.com/random/?person ${userName}`
+                : userPictureSrc
+            }
+            width={40}
+            height={40}
+            alt={`Profilphoto of ${userName}`}
+          ></UserProfilPhoto>
+          <p>{userName}</p>
+        </UserContainer>
+        <Costs> Costs: {adCosts} Karmapoints</Costs>
+      </Description>
       <DateContainer>created at {createdDate}</DateContainer>
     </StyledArticle>
   );
@@ -45,7 +47,7 @@ export default function AdListItems({
 
 const ImageContainer = styled.section`
   position: relative;
-  height: 35vh;
+  min-height: 50vw;
   width: 100%;
 `;
 
@@ -53,7 +55,7 @@ const StyledArticle = styled.article`
   margin: 2rem;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: ;
   box-shadow: 2px 2px 5px 1px rgba(150, 138, 144, 0.2);
 `;
 
@@ -71,8 +73,16 @@ const UserProfilPhoto = styled(Image)`
 const DateContainer = styled.p`
   color: grey;
   font-size: 0.75rem;
+  text-align: center;
 `;
 
 const Costs = styled.p`
   font-weight: bold;
+`;
+
+const Description = styled.section`
+  display: flex;
+  justify-content: space-between;
+  margin-left: 1rem;
+  margin-right: 1rem;
 `;
--- pages/AdListPage/index.js
+++ pages/AdListPage/index.js
@@ -1,13 +1,14 @@
 import AdListItems from "../../components/AdListItems";
 import useSWR from "swr";
 import { fetcher } from "../../helpers/api";
+import styled from "styled-components";
 
 export default function AdListPage() {
   const { data: ads, error } = useSWR("/api/ads", fetcher);
 
-  if (error) return <h1>There was an error</h1>;
+  if (error) return <h1>Cannot load ads</h1>;
 
-  if (!ads) return <h1>...Loading...</h1>;
+  if (!ads) return <h1>...please wait while loading...</h1>;
 
   ads.sort((a, b) => new Date(b.createdDate) - new Date(a.createdDate));
   const heroCounter = ads.length;

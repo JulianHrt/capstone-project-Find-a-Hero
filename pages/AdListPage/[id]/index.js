97203ad439f60fe64fca4d272778e5be6fa588ac
Julian Hirt <julian.hirt94@web.de>
2022-11-24 18:24:37 +0100

    extract icons as an component , higlighted icons on active path

--- pages/AdListPage/[id]/index.js
+++ pages/AdListPage/[id]/index.js
@@ -5,6 +5,7 @@ import { fetcher } from "../../../helpers/api";
 import styled from "styled-components";
 import Link from "next/link";
 import { useState } from "react";
+import Icons from "../../../components/Icons";
 
 export default function AdDetailsPage() {
   const [isModalShown, setModalShown] = useState(false);
@@ -46,17 +47,13 @@ export default function AdDetailsPage() {
           Ad
         </UserName>
         <Link href={`/AdListPage/${id}/EditAd/`}>
-          <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40">
-            <path d="M19.875 35.125v-3.333l8.917-8.917 3.333 3.333-8.917 8.917ZM5.042 26.583v-3H17.75v3ZM33.833 24.5 30.5 21.167l1.167-1.125q.416-.417 1.041-.438.625-.021 1.042.438l1.208 1.208q.459.417.438 1.042-.021.625-.438 1.041ZM5.042 19.583v-3.041H24.5v3.041Zm0-7v-3H24.5v3Z" />
-          </svg>
+          <Icons variant="edit" color="black" />
         </Link>
         <ButtonAsIcon
           type="button"
           onClick={() => setModalShown(!isModalShown)}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40">
-            <path d="M11.125 35Q9.875 35 9 34.125T8.125 32V9.333H6.25v-3h8.292V4.792H25.5v1.541h8.292v3h-1.875V32q0 1.208-.896 2.104-.896.896-2.146.896Zm17.75-25.667h-17.75V32h17.75ZM15.167 28.625H18V12.667h-2.833Zm6.833 0h2.833V12.667H22ZM11.125 9.333V32Z" />
-          </svg>
+          <Icons variant="delete" color="black" />
         </ButtonAsIcon>
       </UserContainer>
       {isModalShown && (
